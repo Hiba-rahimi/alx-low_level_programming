81a9029c0e21fe197089c3b23173181b6fdff414
Hiba-rahimi <hiba.rahimi20@gmail.com>
2023-03-17 02:05:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry poit
+ * main - Entry point
  * Return: ALways 0 (Success)
  */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
