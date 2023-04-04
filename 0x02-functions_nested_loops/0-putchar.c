10e140d3fb9141bceedc7bfc5f64389294ee6c27
Hiba-rahimi <hiba.rahimi20@gmail.com>
2023-04-04 04:30:37 -0700

    This should be the solution of the task #0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char* m ="_putchar\n";
+	write(STDOUT_FILENO, m, strlen(m));
+	return (0);
+}
