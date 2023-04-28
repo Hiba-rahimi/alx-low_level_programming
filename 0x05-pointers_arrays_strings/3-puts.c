06669051c077f2a4cd47460444c5dde4236bfa7b
Hiba-rahimi <hiba.rahimi20@gmail.com>
2023-04-28 00:50:59 -0700

    This should be the solution of task #4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,14 +5,12 @@
   * _puts - prints a string followed by a new line
   * @str: The string to print
   */
-
 void _puts(char *str)
 {
-int i;
-
-	for (i = 0 ; i < strlen(str) ; i++)
-{
-	_putchar(str[i]);
-}
+while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
 _putchar('\n');
 }
