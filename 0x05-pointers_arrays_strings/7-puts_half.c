ecf8983dfec574530e05648093c690236e32a035
Hiba-rahimi <hiba.rahimi20@gmail.com>
2023-04-28 23:16:44 -0700

    This should be the solution of Task #8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * puts_half - prints the last half of a string followed by a new line
+  * @str: The string to print
+  */
+void puts_half(char *str)
+{
+unsigned int i;
+
+	for (i = (strlen(str) - 1) / 2; i < strlen(str) ; i++)
+	{
+		_putchar(str[i]);
+	}
+_putchar('\n');
+}
